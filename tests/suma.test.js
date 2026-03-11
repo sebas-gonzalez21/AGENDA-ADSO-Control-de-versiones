7c0bce3cbb8ddbcbd396160b73f23a037a3cf3d6
Sebas <sebascartagena2006@gmail.com>
2026-03-11 13:52:08 -0500

    chore: revertir main al estado original

--- tests/suma.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-function suma(a, b) {
-  return a + b;
-}
-test('suma 2 + 3 = 5', () => {
-  assert.equal(suma(2, 3), 5);
-});
